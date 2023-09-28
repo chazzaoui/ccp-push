b29865e7d72001dc98e513073614e57a774061f2
chaffie <c.azzaoui@live.nl>
2023-09-27 20:22:30 -0400

    (fix)

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang='en'>
       <head>
         <link rel='manifest' href='/manifest.json' />
-        <link rel='apple-touch-icon' href='/icon-512x512.png'></link>
+        <link rel='apple-touch-icon' href='image/icon-512x512.png'></link>
         <meta name='theme-color' content='#000' />
       </head>
       <body className={inter.className}>{children}</body>
