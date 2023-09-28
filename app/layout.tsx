aaccf0621815018682a4b44290e330a1dc020c6f
chaffie <c.azzaoui@live.nl>
2023-09-27 20:17:45 -0400

    (chore): add manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,11 @@ export default function RootLayout({
 
   return (
     <html lang='en'>
+      <head>
+        <link rel='manifest' href='/manifest.json' />
+        <link rel='apple-touch-icon' href='/icon-512x512.png'></link>
+        <meta name='theme-color' content='#000' />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
