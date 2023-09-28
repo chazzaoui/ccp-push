2a5110f5922470cdf4450e5959345d5a1b15c8bf
chaffie <c.azzaoui@live.nl>
2023-09-27 20:13:58 -0400

    (feat): first feat

---
+++ firebase.js
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDXNQBE9GilhysQ2fPgotfbvh4YjN8nxDI',
+  authDomain: 'ccp-push.firebaseapp.com',
+  projectId: 'ccp-push',
+  storageBucket: 'ccp-push.appspot.com',
+  messagingSenderId: '712283960458',
+  appId: '1:712283960458:web:bfaa8b4cd1ca5720ae6973',
+  measurementId: 'G-80BXFCQ0EN'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const firebaseApp = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+export const getUserToken = setTokenFound => {
+  return getToken(messaging, { vapidKey: 'GENERATED_MESSAGING_KEY' })
+    .then(currentToken => {
+      if (currentToken) {
+        console.log('current token for client: ', currentToken);
+        setTokenFound(true);
+        // Track the token -> client mapping, by sending to backend server
+        // show on the UI that permission is secured
+      } else {
+        console.log(
+          'No registration token available. Request permission to generate one.'
+        );
+        setTokenFound(false);
+        // shows on the UI that permission is required
+      }
+    })
+    .catch(err => {
+      console.log('An error occurred while retrieving token. ', err);
+      // catch error while creating client token
+    });
+};
---
+++ pushnotification.tsx
@@ -0,0 +1,50 @@
+import { initializeApp } from 'firebase/app';
+import { getMessaging } from 'firebase/messaging';
+
+const app = initializeApp({
+  apiKey: 'AIzaSyDXNQBE9GilhysQ2fPgotfbvh4YjN8nxDI',
+  authDomain: 'ccp-push.firebaseapp.com',
+  projectId: 'ccp-push',
+  storageBucket: 'ccp-push.appspot.com',
+  messagingSenderId: '712283960458',
+  appId: '1:712283960458:web:bfaa8b4cd1ca5720ae6973',
+  measurementId: 'G-80BXFCQ0EN'
+});
+
+export const initializeFirebase = () => {
+  initializeApp({
+    apiKey: 'AIzaSyDXNQBE9GilhysQ2fPgotfbvh4YjN8nxDI',
+    authDomain: 'ccp-push.firebaseapp.com',
+    projectId: 'ccp-push',
+    storageBucket: 'ccp-push.appspot.com',
+    messagingSenderId: '712283960458',
+    appId: '1:712283960458:web:bfaa8b4cd1ca5720ae6973',
+    measurementId: 'G-80BXFCQ0EN'
+  });
+};
+
+export function NotificationButton() {
+  const askForPermissionToReceiveNotifications = async () => {
+    try {
+      const messaging = getMessaging(app);
+
+      console.log('Requesting permission...');
+      const permission = await Notification.requestPermission();
+
+      if (permission === 'granted') {
+        console.log('Notification permission granted.');
+        // You can now use messaging to handle notifications
+      } else {
+        console.log('Notification permission not granted.');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <button onClick={askForPermissionToReceiveNotifications}>
+      Click to receive notifications
+    </button>
+  );
+}
